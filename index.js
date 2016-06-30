b9901335f1fd2e38f8d5c326e4c55b2f28a6c438
afei <afeiszh@163.com>
2016-06-30 16:52:11 +0800

    修改

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+'use strict';
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+var redisService = require('./redis');
 app.use( urlencodedParser);
-app.get('/self.pac', function (req, res) {
-  var host = req.query.host || '127.0.0.1';
-  var port = req.query.port || '8888';
-  var tpl =  `function FindProxyForURL(url, host) {
-      return "PROXY ${host}:{$port}";
-  };`
-  res.send(tpl);
+app.get('/pac/:user', function (req, res) {
+  let user = req.params.user.slice(0,-4) || 'default';
+  console.log(user)
+  redisService.hgetall(user).then(function(reply){
+    console.log(reply)
+    let host = reply.host;
+    let port = reply.port;
+    let tpl =  `function FindProxyForURL(url, host) {
+      return "PROXY ${host}:${port}; DIRECT";
+    };`
+    res.send(tpl);
+  })
+  .catch(function(err){
+    let tpl =  `function FindProxyForURL(url, host) {
+      return "DIRECT";
+    };`
+    res.send(tpl);
+  })
+
 });
 app.get('/setpac', function (req, res) {
-
-  res.send('Hello World!');
+  var user = req.query.user || 'default';
+  var port = req.query.port || '8888';
+  var host = req.query.host || '127.0.0.1';
+  var port = req.query.port || '8888';
+  redisService.hmset(user,['host',host,'port',port]).then(function(reply){
+    res.send('success');
+  })
+  .catch(function(err){
+    console.log(err)
+    res.send('fail')
+  })
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
---
+++ redis.js
@@ -0,0 +1,73 @@
+var redis = require("redis"),
+    client = redis.createClient();
+var isConnect = false;
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+client.on("ready", function () {
+  isConnect = true;
+});
+var RedisService ={};
+RedisService.hset = function(key,name,value) {
+  return new Promise(function(reslove,reject){
+    if (!isConnect) {
+      reject(new Error('redis连接失败'));
+    }
+    client.hset(key,name,value,function(err,reply){
+      if(err){
+        reject(err);
+      }
+      else {
+        reslove(reply);
+      }
+    })
+  })
+}
+RedisService.hmset = function(key,args) {
+  return new Promise(function(reslove,reject){
+    if (!isConnect) {
+      reject(new Error('redis连接失败'));
+    }
+    args.unshift(key);
+    client.hmset(args,function(err,reply){
+      if(err){
+        reject(err);
+      }
+      else {
+        reslove(reply);
+      }
+    })
+  })
+}
+RedisService.hget = function(key,name) {
+  return new Promise(function(reslove,reject){
+    if (!isConnect) {
+      reject(new Error('redis连接失败'));
+    }
+    client.hset(key,name,function(err,reply){
+      if(err){
+        reject(err);
+      }
+      else {
+        reslove(reply);
+      }
+    })
+  })
+}
+RedisService.hgetall = function(key) {
+  return new Promise(function(reslove,reject){
+    if (!isConnect) {
+      reject(new Error('redis连接失败'));
+    }
+    client.hgetall(key,function(err,reply){
+      if(err){
+        reject(err);
+      }
+      else {
+        reslove(reply);
+      }
+    })
+  })
+}
+module.exports = RedisService;
